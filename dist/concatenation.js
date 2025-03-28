b56dbb5d50a4043a164122bf3ad7b0994c9f1fe7
Наталия Левенец <N.Levenetsy@lot.com>
2025-03-28 12:36:00 +0200

    Remove dist folder from repository

--- dist/concatenation.js
+++
@@ -1,5 +0,0 @@
-function concatenation(firstWord, secondWord, num = 10) {
-    console.log(`${firstWord} ${secondWord}, from ${num}`);
-}
-export { concatenation };
-//# sourceMappingURL=concatenation.js.map
