1cfa76d1e8d045ee404865a45e08e71438f16e5a
Наталия Левенец <N.Levenetsy@lot.com>
2025-03-28 12:27:19 +0200

    some changes in gitignore

---
+++ dist/concatenation.js
@@ -0,0 +1,5 @@
+function concatenation(firstWord, secondWord, num = 10) {
+    console.log(`${firstWord} ${secondWord}, from ${num}`);
+}
+export { concatenation };
+//# sourceMappingURL=concatenation.js.map
